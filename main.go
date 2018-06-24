bf0164f0c0e1e1d46a069521f7334b1bf9458784
Chung HD <chunghd126@gmail.com>
2018-06-24 17:12:31 +0700

    move routers to another file

--- main.go
+++ main.go
@@ -6,9 +6,7 @@ import (
 	"net/http"
 	"fmt"
 	"os"
-	"ielts_study_backend/controllers"
-	"ielts_study_backend/controllers/api/word"
-	"github.com/gorilla/mux"
+
 )
 
 func main() {
@@ -20,36 +18,5 @@ func main() {
 		err != nil {
 		panic(err)
 	}
-}
-
-type RouteElement struct {
-	Methods []string
-	Handler interface{}
-}
-
-func Route(handler interface{}, method []string) RouteElement {
-	if len(method) == 0  {
-		method = []string{"GET", "POST", "PUT"} // default handler will serve 3 methods: GET, POST, PUT
-	}
-	fmt.Println(method)
-	return RouteElement{method, handler}  // enforce the default value here
-}
-
-func routeApi()  {
-
-	routes := map[string]RouteElement{
-		"/" : Route(controllers.Index, []string{}),
-
-		// Routes for word apis
-		"/api/word" : Route(word.HandleGet, []string{}),
-	}
-
-	router := mux.NewRouter()
-
-	for route, handler := range routes  {
-		router.HandleFunc(route, handler.Handler.(func(w http.ResponseWriter, r *http.Request))).Methods(handler.Methods...)
-	}
-
-	http.Handle("/", router)
-	return
+	defer models.CloseDB()
 }
---
+++ routers.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"ielts_study_backend/controllers/api/word"
+	"github.com/gorilla/mux"
+	"net/http"
+	"ielts_study_backend/controllers"
+)
+
+type RouteElement struct {
+	Methods []string
+	Handler interface{}
+}
+
+func Route(handler interface{}, method []string) RouteElement {
+	if len(method) == 0  {
+		method = []string{"GET", "POST", "PUT"} // default handler will serve 3 methods: GET, POST, PUT
+	}
+	return RouteElement{method, handler}  // enforce the default value here
+}
+
+func routeApi()  {
+
+	routes := map[string]RouteElement{
+		"/" : Route(controllers.Index, []string{}),
+
+		// Routes for word apis
+		"/api/word" : Route(word.HandleGet, []string{}),
+	}
+
+	router := mux.NewRouter()
+
+	for route, handler := range routes  {
+		router.HandleFunc(route, handler.Handler.(func(w http.ResponseWriter, r *http.Request))).Methods(handler.Methods...)
+	}
+
+	http.Handle("/", router)
+	return
+}
